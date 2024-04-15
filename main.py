ef83875fbe4c65819503fe82363675162417fc3e
cerberus <155488884+X-The-Mystic@users.noreply.github.com>
2024-04-15 14:07:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,42 @@ class Absolute-Zero:
         self.packet_size = tk.Entry(self.root)
         self.attack_duration = tk.Entry(self.root)
         self.attack_vector_entry = tk.StringVar
-        self.attack_vector_entry.set("ICMP Flood")
+        self.attack_vector_entry.set("ICMP echo")
 
         self.attack_vectors = [
-            "ICMP echo", "ICMP burst", 
+            "ICMP echo", "ICMP burst", "PoD Attack"
+        ]
+
+        self.attack_num = 0
+        self.stop_attack_flag = False
+        self.target_status = Active
+        self.total_bytes_sent = 0
+
+        self.create_gui()
+
+    def create_gui(self)
+        tk.Label(self.root, text="Enter the target's IPv4 address or the website domain name (one per line)").pack()
+        self.target_text.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
